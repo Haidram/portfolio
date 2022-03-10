47ef55001ebc029944e612de25544f70c36fc79a
Md Rameez <83173390+Haidram@users.noreply.github.com>
2022-03-10 21:42:14 +0530

    deleted the javascript file

--- app.js
+++
@@ -1,8 +0,0 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementById('nav_List')
-
-function toggleButton(){
-	navList.classList.toggle('show')
-}
-
-hamburgerButton.addEventListener('click', toggleButton)
